b0fee4d9d2a4c66439a1f8b379ef95c4930c48dc
deeveraEnchongOfficial <deveraflorencio18@gmail.com>
2024-02-08 08:35:27 +0800

    first commit

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>My Project</span>,
+  // project: {
+  //   link: 'https://github.com/deeveraEnchongOfficial/app-doc',
+  // },
+  // chat: {
+  //   link: '#',
+  // },
+  docsRepositoryBase: 'https://github.com/deeveraEnchongOfficial/app-doc',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
